1ad94d031fee955b05db553b7af6d86e84078f7d
alphabravo2k-rgb <alphabravo2k@gmail.com>
2025-12-30 16:09:50 +0500

    index.js

---
+++ src/components/admin/index.js
@@ -0,0 +1,12 @@
+export { AdminDashboard } from './AdminDashboard';
+export { AdminToolbar } from './AdminToolbar';
+export { AdminMatchControls } from './AdminMatchControls';
+export { AdminMatchModal } from './AdminMatchModal';
+export { AdminRosterReview } from './AdminRosterReview';
+export { StaffManagement } from './StaffManagement';
+export { TeamStatusControl } from './TeamStatusControl';
+export { MatchFormatControl } from './MatchFormatControl';
+export { RosterIntegrityControl } from './RosterIntegrityControl';
+export { AdminLogin } from './AdminLogin';
+export { BracketSwapper } from './BracketSwapper';
+// Add this line if you move MatchRoom to admin folder, otherwise import from match/MatchRoom
